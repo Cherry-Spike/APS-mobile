7615335dc4877676750ee080acb73d7ba0ef4316
everton42 <evertonferreira4596@gmail.com>
2021-10-09 15:39:03 -0300

    loading

--- src/components/card/PollutionCard.vue
+++ src/components/card/PollutionCard.vue
@@ -1,18 +1,15 @@
 <template>
   <ion-card>
-    <ion-searchbar
-      type="text"
-      debounce="0"
-      :value="searchInput"
-      @ionInput="
-        searchInput = $event.target.value;
-        onInput$.next($event.target.value);
-      "
-      @ionClear="searchCleared($event)"
-      placeholder="Digite a cidade"
-    >
+    <ion-searchbar type="text" debounce="0" placeholder="Digite a cidade">
     </ion-searchbar>
   </ion-card>
+  <ion-loading
+    :is-open="isOpenRef"
+    message="Carregando..."
+    :duration="isLoaded == false"
+    @didDismiss="setOpen(false)"
+  >
+  </ion-loading>
   <ion-card v-if="isLoaded">
     <ion-card-header>
       <ion-card-subtitle>Cidade</ion-card-subtitle>
@@ -20,11 +17,27 @@
     </ion-card-header>
     <ion-card-content>
       <ion-list>
-        <ion-item
-          v-for="(data, i) of pollutinData.forecast.daily.o3"
-          v-bind:key="i"
-        >
-          {{ data.avg }}
+        <ion-list-header>Informaçoẽs atmosféricas</ion-list-header>
+
+        <ion-item>
+          <ion-label>AQI</ion-label>
+          <ion-note slot="end">{{ pollutionData.aqi }}</ion-note>
+        </ion-item>
+        <ion-item>
+          <ion-label>NO2</ion-label>
+          <ion-note slot="end">{{ pollutionData.iaqi.no2.v }}</ion-note>
+        </ion-item>
+        <ion-item>
+          <ion-label>O3</ion-label>
+          <ion-note slot="end">{{ pollutionData.iaqi.o3.v }}</ion-note>
+        </ion-item>
+        <ion-item>
+          <ion-label>PM10</ion-label>
+          <ion-note slot="end">{{ pollutionData.iaqi.pm10.v }}</ion-note>
+        </ion-item>
+        <ion-item>
+          <ion-label>PM25</ion-label>
+          <ion-note slot="end"> {{ pollutionData.iaqi.pm25.v }}</ion-note>
         </ion-item>
       </ion-list>
     </ion-card-content>
@@ -41,8 +54,11 @@ import {
   IonSearchbar,
   IonList,
   IonItem,
+  IonListHeader,
+  IonNote,
+  IonLoading,
 } from "@ionic/vue";
-import { defineComponent } from "vue";
+import { defineComponent, ref } from "vue";
 import axios from "axios";
 export default defineComponent({
   name: "PollutionCard",
@@ -55,9 +71,19 @@ export default defineComponent({
     IonSearchbar,
     IonList,
     IonItem,
+    IonListHeader,
+    IonNote,
+    IonLoading,
+  },
+  setup() {
+    const isOpenRef = ref(false);
+    const setOpen = (state) => (isOpenRef.value = state);
+
+    return { isOpenRef, setOpen };
   },
   data: () => ({ pollutionData: {}, isLoaded: false }),
   mounted() {
+    this.setOpen(true);
     this.getCity("Jundiai");
   },
   methods: {
@@ -68,6 +94,7 @@ export default defineComponent({
         )
         .then((response) => {
           this.isLoaded = true;
+          this.setOpen(false);
           this.pollutionData = response.data.data;
         })
         .catch((error) => console.log(error));
